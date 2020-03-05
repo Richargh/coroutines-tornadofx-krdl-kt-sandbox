b962056b17e17bfec505c3440658ac6aac1d12d6
Richard Gross <richard.gross@maibornwolff.de>
2020-03-05 23:46:19 +0100

    divided cornado main into different areas with borderpane

---
+++ src/main/kotlin/de/richargh/sandbox/cornado/HeaderView.kt
@@ -0,0 +1,9 @@
+package de.richargh.sandbox.cornado
+
+import tornadofx.*
+
+class Header : View() {
+
+    override val root = vbox {
+    }
+}
--- src/main/kotlin/de/richargh/sandbox/cornado/MainView.kt
+++ src/main/kotlin/de/richargh/sandbox/cornado/MainView.kt
@@ -1,76 +1,13 @@
 package de.richargh.sandbox.cornado
 
-import javafx.beans.property.SimpleDoubleProperty
-import javafx.scene.control.TextField
-import javafx.scene.paint.Color
-import kotlinx.coroutines.*
-import tornadofx.*
-import java.time.LocalDate
-import kotlinx.coroutines.javafx.JavaFx
+import tornadofx.View
+import tornadofx.borderpane
 
-class MainView: CoroutineScope, View() {
+class MainView: View() {
 
-    override val coroutineContext = SupervisorJob() +
-                                    Dispatchers.Default +
-                                    CoroutineName(javaClass.simpleName)
-
-    private val events: Events by inject()
-
-    var firstNameField: TextField by singleAssign()
-    var lastNameField: TextField by singleAssign()
-    var ageField: TextField by singleAssign()
-
-    private val personsViewModel = PersonsViewModel()
-
-    init {
-        events.personAdded.subscribe { personsViewModel.add(it) }
-    }
-
-    override val root = vbox {
-        hbox {
-            label("First Name")
-            firstNameField = textfield()
-        }
-        hbox {
-            label("Last Name")
-            lastNameField = textfield()
-        }
-        hbox {
-            label("Age")
-            ageField = textfield()
-        }
-        button("ADD") {
-            useMaxWidth = true
-            action {
-                info("On Add")
-                launch {
-                    info("Heavy Computation")
-                    delay(2000)
-                    val person = PersonViewModel(firstNameField.text, lastNameField.text, LocalDate.of(2020, 8, 11))
-                    withContext(Dispatchers.Main){
-                        info("Pushing Event")
-                        events.personAdded.push(person)
-                    }
-                }
-            }
-        }
-        val completion = SimpleDoubleProperty(0.5)
-        progressindicator(completion) {}
-
-        tableview(personsViewModel.items) {
-            column("First Name", PersonViewModel::firstnameProperty)
-            column("Second Name", PersonViewModel::lastnameProperty)
-            column("Birthday", PersonViewModel::birthdayProperty)
-            column("Age", PersonViewModel::ageProperty).cellFormat {
-                text = it.toString()
-                style {
-                    backgroundColor += if (it != null && it < 18) {
-                        c("#8b0000")
-                    } else {
-                        Color.TRANSPARENT
-                    }
-                }
-            }
-        }
+    override val root = borderpane {
+        top<Header>()
+        left<CustomerForm>()
+        center<CenterView>()
     }
 }
