69ee47e9f117afe7fb92045cd09a607d79b34388
Richard Gross <richard.gross@maibornwolff.de>
2020-03-06 00:46:31 +0100

    extended both samples a lot

---
+++ src/main/kotlin/de/richargh/sandbox/aminisample/MiniComplexConsoleSample.kt
@@ -0,0 +1,38 @@
+package de.richargh.sandbox.aminisample
+
+import de.richargh.sandbox.cornado.info
+import kotlinx.coroutines.*
+import java.io.IOException
+import java.net.URL
+
+fun main() = runBlocking {
+    compute("What about whatism?")
+    val fooTitle = searchTitleAsync("Foo")
+    val fubarTitle = searchTitleAsync("Fubar")
+
+    info("Foo is [${fooTitle.await()}]")
+    info("Foo is [${fubarTitle.await()}]")
+}
+
+fun CoroutineScope.compute(question: String) = launch(Dispatchers.Default) {
+    info("Before Heavy computation")
+    delay(2000)
+    info("The answer is always [42]")
+}
+
+fun CoroutineScope.searchTitleAsync(term: String): Deferred<String?> {
+    return async(Dispatchers.IO) {
+        info("Searching")
+        val response: String? = try {
+            URL("https://en.wikipedia.org/w/index.php?search=$term")
+                    .openStream()
+                    .bufferedReader()
+                    .use { it.readText() }
+        } catch (ex: IOException){
+            info("Request failed: $ex")
+            null
+        }
+
+        response?.split("<title>")?.get(1)?.split("</title>")?.get(0)
+    }
+}
--- src/main/kotlin/de/richargh/sandbox/aminisample/MiniConsoleSample.kt
+++ src/main/kotlin/de/richargh/sandbox/aminisample/MiniConsoleSample.kt
@@ -1,7 +1,62 @@
 package de.richargh.sandbox.aminisample
 
-import kotlinx.coroutines.runBlocking
+import de.richargh.sandbox.cornado.info
+import kotlinx.coroutines.*
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicInteger
 
-fun main() = runBlocking {
-
+fun main() {
+    info("Before CoroutineScope")
+    // enter coroutine land
+    enter()
+    info("After CoroutineScope")
 }
+
+// to enter coroutine land we need a the runBlocking builder that creates a coroutine
+// this coroutine blocks the thread until it completes
+// inside the coroutine we can now launch many other coroutines though
+fun enter() = runBlocking {
+    info("Before counting")
+    val count = counting()
+    info("After counting [$count]")
+    info("Before incrementing")
+    val inc = increment().await()
+    info("After incrementing [$inc]")
+}
+
+suspend fun counting(): Int = coroutineScope {
+    val data = ConcurrentLinkedQueue<Int>()
+    repeat(1000){ number ->
+        launch {
+            // DON'T call Thread.Sleep, that would block all Coroutines on this thread
+            // call delay instead
+            delay(500)
+            data.add(number)
+            if(number == 500) info("Count number $number")
+            if(number == 999) info("Countnumber $number")
+        }
+    }
+
+    info("I'll wait until everything has finished")
+    data.size
+}
+
+fun CoroutineScope.increment(): Deferred<AtomicInteger> {
+    return async {
+        val acc = AtomicInteger(0)
+        repeat(1000){ number ->
+            launch {
+                // DON'T call Thread.Sleep, that would block all Coroutines on this thread
+                // call delay instead
+                delay(500)
+                acc.incrementAndGet()
+                if(number == 500) info("Increment number $number")
+                if(number == 999) info("Increment number $number")
+            }
+        }
+
+        info("I'll wait until everything has finished")
+        acc
+    }
+}
+
