89ceaf4d18d6d8f38fd03c09f0f910dfa10eed62
Richard Gross <richard.gross@maibornwolff.de>
2020-03-08 00:26:48 +0100

    add logging method that prints coroutine name

--- src/main/kotlin/de/richargh/sandbox/cornado/Logging.kt
+++ src/main/kotlin/de/richargh/sandbox/cornado/Logging.kt
@@ -1,3 +1,7 @@
 package de.richargh.sandbox.cornado
 
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+
 fun info(message: Any?) = println("[${Thread.currentThread().name}] $message")
+fun CoroutineScope.info(message: Any?) = println("[${Thread.currentThread().name}@${coroutineContext[CoroutineName]?.name ?: "NoName"} ] $message")
